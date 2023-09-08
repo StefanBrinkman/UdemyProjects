1640e95527078e4a38724bcf598dfa43292854ce
Stefan-Brinkman <stefan.brinkman@cobbler.nl>
2023-09-08 13:09:03 +0200

    Added soundPool => Lecture 269 udemy android

--- AndroidAppDevelopmentMasterclass/MediaPlayer/app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ AndroidAppDevelopmentMasterclass/MediaPlayer/app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.mediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioAttributes;
 import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,83 +15,98 @@ import android.widget.Toast;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
-    private MediaPlayer mediaPlayer;
-    private Button playButton;
-    private SeekBar seekbar;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private String soundEffect1 = "http://codeskulptor-demos.commondatastorage.googleapis.com/descent/Zombie.mp3";
+    private Button buttonOne, buttonTwo, buttonThree, buttonFour;
+
+    private String soundEffect1 = String.valueOf(Uri.parse("http://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a"));
     private String soundEffect2 = "http://codeskulptor-demos.commondatastorage.googleapis.com/descent/background%20music.mp3";
     private String soundEffect3 = "http://codeskulptor-demos.commondatastorage.googleapis.com/descent/spring.mp3";
     private String soundEffect4 = "http://codeskulptor-demos.commondatastorage.googleapis.com/descent/gotitem.mp3";
+    private int sound1, sound2, sound3, sound4;
+
+    private boolean soundLoaded = false;
+
+    private SoundPool soundPool;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        playButton = findViewById(R.id.playButton);
-        mediaPlayer = new MediaPlayer();
-        seekbar = findViewById(R.id.seekBarId);
+        buttonOne = findViewById(R.id.buttonOne);
+        buttonTwo = findViewById(R.id.buttonTwo);
+        buttonThree = findViewById(R.id.buttonThree);
+        buttonFour = findViewById(R.id.buttonFour);
 
+        buttonOne.setOnClickListener(this);
+        buttonTwo.setOnClickListener(this);
+        buttonThree.setOnClickListener(this);
+        buttonFour.setOnClickListener(this);
+
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                .build();
+
+        soundPool = new SoundPool.Builder()
+                .setMaxStreams(4)
+                .setAudioAttributes(audioAttributes)
+                .build();
+
+        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                Log.d("soundPool", "onLoadComplete: ");
+                soundLoaded = true;
+            }
+        });
         try {
-            mediaPlayer.setDataSource("https://buildappswithpaulo.com/music/watch_me.mp3");
-        } catch (IOException e) {
-            e.printStackTrace();
+            sound1 = soundPool.l
+            Log.d("Sound 1", "onCreate: " + sound1);
+        } catch (Exception error) {
+            Log.d("LOADING ERROR", "onLoad: " + error);
         }
+//        sound2 = soundPool.load(soundEffect2, 1);
+//        sound3 = soundPool.load(soundEffect3, 1);
+//        sound4 = soundPool.load(soundEffect4, 1);
+    }
 
-        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                int duration = mp.getDuration();
-                // TOAST
-            }
-        });
-        MediaPlayer.OnPreparedListener preparedListener = new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                mp.start();
-                seekbar.setMax(mediaPlayer.getDuration());
-                playButton.setOnClickListener(view -> {
-                    if(mp.isPlaying()) {
-                        mp.pause();
-                        playButton.setText("Play");
-                    } else {
-                        mp.start();
-                        playButton.setText("Pause");
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonOne:
+                    if(soundLoaded) {
+                        soundPool.play(sound1,1,1,0,1,1);
                     }
-                });
-            }
-        };
+                break;
 
-        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.d("Seekbar", "Progress change");
-                if(fromUser) {
-                    mediaPlayer.seekTo(progress);
+            case R.id.buttonTwo:
+                if(soundLoaded) {
+                    soundPool.play(sound2,1,1,0,1,1);
                 }
-            }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                Log.d("Seekbar", "Dragging started");
-            }
+                break;
 
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                Log.d("Seekbar", "Dragging ended on: " + seekBar.getProgress());
-            }
-        });
-        mediaPlayer.setOnPreparedListener(preparedListener);
-        mediaPlayer.prepareAsync();
+            case R.id.buttonThree:
+                if(soundLoaded) {
+                    soundPool.play(sound3,1,1,0,1,1);
+                }
+                break;
+
+            case R.id.buttonFour:
+                if(soundLoaded) {
+                    soundPool.play(sound4,1,1,0,1,1);
+                }
+                break;
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(mediaPlayer != null) {
-            mediaPlayer.pause();
-            mediaPlayer.release();
+        if(soundPool != null) {
+            soundPool.release();
+            soundPool = null;
         }
     }
 }
